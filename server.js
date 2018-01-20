2cca939ce51afc7b0a8badb9ab55e7f183fe6480
Srijayanth Sridhar <srijayanth@gmail.com>
2018-01-20 10:33:59 +0530

    Server now fetches data from Github and serves

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const getPairMappings = require('./decryptPairMapping.js').getPairMappings;
 const constructQuery = require('./queryConstructor.js').constructQuery;
+const GithubQuery = require('./githubQuery.js');
 
 const PORT=process.env.PORT || 8080;
 const pairMapFile=process.env.PAIR_MAP || "./users";
 const pairMap=getPairMappings(pairMapFile);
 const org=process.env.ORG || "STEP-tw";
+const token=process.env.GITHUB_TOKEN;
 
 const query=constructQuery(pairMap,org,"./graphql_query");
-console.log(query);
+
+let data={data:{}};
+const githubQuery=new GithubQuery(token,query,(body)=>{
+  console.log("Received update...");
+  data=JSON.parse(body);
+});
+
+if(!process.env.SERVE_DATA_LOCAL)
+  githubQuery.fetch();
 
 const app = express();
 app.use(express.static("public"));
-
+if(process.env.SERVE_DATA_LOCAL) {
+  console.log("serving data locally");
+  app.get('/data.json',express.static("public/data"));
+}
+app.get('/data.json',(req,res)=>{
+  res.setHeader("Content-Type","application/json");
+  res.write(JSON.stringify(data));
+  res.end();
+})
 app.listen(PORT,()=>console.log(`listening on ${PORT}`));
